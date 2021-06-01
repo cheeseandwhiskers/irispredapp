9ae15a1ad38be81c6de165201253557fee65d2ab
unknown <goh_weibin01.pt@ichat.sp.edu.sg>
2021-06-02 02:13:21 +0800

    Update predict.html

--- config/Settings.py
+++ config/Settings.py
@@ -3,13 +3,13 @@ import os
 class Settings:
     secret="sajdjsah#@!$#cxbcxbn(*12ssap[s"
     
-    #Heroku database settings
+    # Heroku database settings
     host=os.environ['HOST']
     database=os.environ['DATABASE']
     user=os.environ['USERNAME']
     password=os.environ['PASSWORD']
 
-    #local dev database settings
+    # #local dev database settings
     # host='localhost'
     # database='iris_db'
     # user='root'
